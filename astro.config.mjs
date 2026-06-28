35fb28a65df22c1881d338db93f4a2b238a1a0d5
alessandro di fabio <37181764+AleDifa@users.noreply.github.com>
2026-06-28 23:11:18 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://alessandrodifa.com',
+  vite: {
+    server: {
+      allowedHosts: true
+    }
+  }
 });
