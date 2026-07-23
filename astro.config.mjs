abc46f8448344ebc4ee9e293d6b737f025721831
AleDifa <alessandro.difa93@gmail.com>
2026-07-23 12:00:13 +0200

    nuovo sfondo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://alessandrodifa.com',
+
   vite: {
     server: {
       allowedHosts: true
     }
-  }
+  },
+
+  integrations: [react()]
 });
